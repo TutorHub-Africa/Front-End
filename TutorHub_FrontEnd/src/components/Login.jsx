5ed193297dbdd86966419ab0f6f6714ee10da450
fedhako7 <135381942+fedhako7@users.noreply.github.com>
2024-07-14 22:21:40 +0300

    Fixed buttons in Login.jsx

--- TutorHub_FrontEnd/src/components/Login.jsx
+++ TutorHub_FrontEnd/src/components/Login.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
 import '../styles/login.css'
-
-import email_logo from '../assets/email logo.jpg'
-import pers_logo from '../assets/person logo.png'
-import pass_logo from '../assets/password logo.png'
 import google_logo from '../assets/google_logo.png'
 
 export const Login = () => {
@@ -13,18 +9,17 @@ export const Login = () => {
             <div className="student">Student Login</div>
             <div className="links">
                 <div>Home</div>
-                <div>></div>
+                <div>&gt;</div>
                 <div className="login">Login</div>
             </div>
         </div>
 
         <div className="middle">
             <div className="welcome">Welcome back!</div>
-            <div className="text">Hey there! Ready to log in? Just enter your username and password below <br />
-            and you'll be back in action in no time. Let's go!</div>
+            <div className="text">Empowering Learners, One Click at a Time!</div>
             <div className="continue-with">
-                <img src={google_logo} alt="" />
-                <p>Continue with Google</p>
+            <a href="#"><img src={google_logo} alt="" />
+                <p>Continue with Google</p></a>
             </div>
             <div className='or'>
                 <div></div>
@@ -44,13 +39,14 @@ export const Login = () => {
             </div>
             <div className="remember">
                 <div>Remember me</div>
-                <div className="forgot">Forgot Password?</div>
+                <a href="#" className="forgot">Forgot Password?</a>
             </div>
 
             <button className="signin">Sign In</button>
             <div className="signup">
                 <div>Don't have an account?</div>
-                <div className='sign-up'>Sign Up</div>
+                <a href="#">Sign Up</a>
+                <div className='sign-up'></div>
             </div>
         </div>
 
